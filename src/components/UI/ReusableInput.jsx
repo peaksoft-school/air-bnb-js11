fa9482e734127f5aebbca7e606e156a1a8ad3ac8
Isaeva Kyial <isaevakiyal05@gmail.com>
2024-01-11 17:08:10 +0600

    delete props

--- src/components/UI/ReusableInput.jsx
+++ src/components/UI/ReusableInput.jsx
@@ -2,36 +2,13 @@ import { TextField, styled } from '@mui/material'
 import React, { forwardRef } from 'react'
 
 const Input = forwardRef(
-   (
-      {
-         type,
-         size,
-         label,
-         name,
-         placeholder,
-         onChange,
-         onClick,
-         value,
-         width,
-         height,
-         error,
-         checked,
-         defaultChecked,
-         ...rest
-      },
-      ref
-   ) => {
+   ({ type, label, placeholder, onChange, onClick, value, ...rest }, ref) => {
       return (
          <StyledInput
-            size={size}
-            name={name}
             type={type}
             label={label}
             value={value}
-            error={error}
-            checked={checked}
             onChange={onChange}
-            defaultChecked={defaultChecked}
             placeholder={placeholder || 'Введите что-нибудь'}
             ref={ref}
             {...rest}
@@ -40,24 +17,24 @@ const Input = forwardRef(
    }
 )
 
-const StyledInput = styled(TextField)(({ text, width, marginBottom }) => ({
-   width: width || '40%',
+const StyledInput = styled(TextField)(() => ({
+   width: '40%',
    backgroundColor: '#fff',
-   marginBottom: marginBottom || '0px',
+   marginBottom: '0px',
 
    '& .MuiOutlinedInput-input': {
       borderRadius: '2px',
    },
    '& .MuiOutlinedInput-root': {
       '& fieldset': {
-         borderColor: text ? '#828282' : 'gray',
+         borderColor: '#828282',
       },
       '&:hover fieldset': {
          border: '2px solid gray',
       },
    },
    '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
-      borderColor: text ? '#828282' : 'gray',
+      borderColor: '#828282',
    },
 }))
 
