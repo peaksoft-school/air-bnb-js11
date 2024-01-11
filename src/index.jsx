dcbc69e3c31b358d13737ffa627307f4dbdaee1e
Isaev Alihan <isaevsali57@gmail.com>
2024-01-11 13:05:41 +0600

    theme-settings

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Themes from './utils/theme/Themes'
+import Themes from './components/UI/theme/Themes'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
