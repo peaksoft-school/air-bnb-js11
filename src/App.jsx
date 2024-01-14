77c3b513a34cc7461df2532f40b8c100a9a28165
Isaev Alihan <isaevsali57@gmail.com>
2024-01-14 19:13:05 +0600

    select

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 import './App.css'
-import Select from './components/UI/Select'
 
 const App = () => {
-   const option = [
-      { id: 1, value: 12 },
-      { id: 2, value: 15 },
-      { id: 3, value: 14 },
-   ]
-   return (
-      <div>
-         Air-bnb
-         <Select
-            label="filter by:"
-            options={option}
-            isValueAsId={false}
-            defaultId="2"
-         />
-      </div>
-   )
+   return <div>Air-bnb</div>
 }
 
 export default App
