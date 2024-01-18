6cc4d3d4dfece87c8943604f4aa89ec088bbfe6d
Bismillah <matraim.kg@gmail.com>
2024-01-18 23:02:28 +0600

    create admin application

---
+++ src/utils/constants/admin-application.js
@@ -0,0 +1,74 @@
+import AdminApplicationImg from '../../assets/images/hotel-mansion.jpg'
+
+const DYMMY_DATA = [
+   {
+      id: 1,
+      images: [{ img: AdminApplicationImg, id: 1 }],
+      title: 'Beautiful and picturesque...',
+      address: '12 Morris Ave, Toronto,...',
+      rating: '3.4',
+      price: '26',
+      guests: 2,
+      type: 'new',
+   },
+   {
+      id: 2,
+      images: [{ img: AdminApplicationImg, id: 2 }],
+      title: 'Beautiful and picturesque...',
+      address: '12 Morris Ave, Toronto,...',
+      rating: '3.4',
+      price: '26',
+      guests: 2,
+   },
+   {
+      id: 3,
+      images: [
+         { img: AdminApplicationImg, id: 31 },
+         { img: AdminApplicationImg, id: 32 },
+         { img: AdminApplicationImg, id: 33 },
+      ],
+      title: 'Beautiful and picturesque...',
+      address: '12 Morris Ave, Toronto,...',
+      rating: '3.4',
+      price: '26',
+      guests: 2,
+   },
+   {
+      id: 4,
+      images: [{ img: AdminApplicationImg, id: 4 }],
+      title: 'Beautiful and picturesque...',
+      address: '12 Morris Ave, Toronto,...',
+      rating: '3.4',
+      price: '26',
+      guests: 2,
+   },
+   {
+      id: 5,
+      images: [{ img: AdminApplicationImg, id: 5 }],
+      title: 'Beautiful and picturesque...',
+      address: '12 Morris Ave, Toronto,...',
+      rating: '3.4',
+      price: '26',
+      guests: 2,
+   },
+   {
+      id: 6,
+      images: [{ img: AdminApplicationImg, id: 6 }],
+      title: 'Beautiful and picturesque...',
+      address: '12 Morris Ave, Toronto,...',
+      rating: '3.4',
+      price: '26',
+      guests: 2,
+   },
+   {
+      id: 7,
+      images: [{ img: AdminApplicationImg, id: 7 }],
+      title: 'Beautiful and picturesque...',
+      address: '12 Morris Ave, Toronto,...',
+      rating: '3.4',
+      price: '26',
+      guests: 2,
+   },
+]
+
+export { DYMMY_DATA }
