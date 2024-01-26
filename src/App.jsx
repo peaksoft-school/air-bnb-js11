d1f0571eb16f00ab1a203f85b0f6193d2547700f
Isaeva Kyial <isaevakiyal05@gmail.com>
2024-01-26 18:55:40 +0600

    created landing-slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+/* eslint-disable import/extensions */
 import './App.css'
+// import LandingSlider from './components/UI/LandingSlider.jsx'
+import PopularApartments from './components/main-page/PopularApartments.jsx'
 
 const App = () => {
-   return <div>AirBnB</div>
+   return (
+      <div>
+         <PopularApartments />
+         {/* <LandingSlider /> */}
+      </div>
+   )
 }
 
 export default App
---
+++ src/utils/constants/index.js
@@ -0,0 +1,19 @@
+import LivingRoom from '../../assets/images/living-room.png'
+import BedRoom from '../../assets/images/bed-room.png'
+import Kitchen from '../../assets/images/kitchen.jpeg'
+import Holl from '../../assets/images/holl.jpeg'
+import ChildRoom from '../../assets/images/child-room.jpeg'
+import BathRoom from '../../assets/images/bath-room.jpg'
+import HellweyRoom from '../../assets/images/hellwey-room.jpg'
+
+const ROOMS = [
+   { img: BedRoom, name: 'bed room' },
+   { img: LivingRoom, name: 'living room' },
+   { img: Kitchen, name: 'kitchen' },
+   { img: Holl, name: 'holl' },
+   { img: ChildRoom, name: 'child room' },
+   { img: BathRoom, name: 'bath room' },
+   { img: HellweyRoom, name: 'hellwey room' },
+]
+
+export { ROOMS }
