8dca1bedab51108a33929291fc7bdcbeac9268e9
Isaeva Kyial <isaevakiyal05@gmail.com>
2024-01-27 18:56:48 +0600

    cleane App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 /* eslint-disable import/extensions */
 import './App.css'
-import PopularApartments from './components/apartment/PopularApartments'
-// import LandingSlider from './components/UI/LandingSlider.jsx'
 
 const App = () => {
-   return (
-      <div>
-         <PopularApartments />
-         {/* <LandingSlider /> */}
-      </div>
-   )
+   return <div>Air-bnb</div>
 }
 
 export default App
