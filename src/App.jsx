a332e792a9be3ed8d2f4a5cc7c4f9bd7889011c4
Isaev Alihan <isaevsali57@gmail.com>
2024-01-31 13:07:58 +0600

    clear app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import { useState } from 'react'
 import './App.css'
-import JoinUs from './components/signIn/JoinUs'
 
 const App = () => {
-   const [open, setOpen] = useState(false)
-
-   const alihan = () => {
-      setOpen((prev) => !prev)
-   }
-   return (
-      <div>
-         <button type="button" onClick={alihan}>
-            ali
-         </button>
-         Air-BnB
-         <JoinUs isOpenModal={open} onClose={alihan} />
-      </div>
-   )
+   return <div>Air-BnB</div>
 }
 
 export default App
