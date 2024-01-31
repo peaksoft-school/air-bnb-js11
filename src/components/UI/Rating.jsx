d0c59902c34ffa555c3110a79917b30a2365a79c
Aziat <abdimalikovaziat@gmail.com>
2024-01-31 13:03:22 +0600

    resolve comments

--- src/components/UI/Rating.jsx
+++ src/components/UI/Rating.jsx
@@ -4,75 +4,81 @@ import { RATINGS } from '../../utils/constants'
 
 const Rating = () => (
    <RatingChart>
-      <RatingCont>
+      <Box className="rating-cont">
          <Typography>4.2</Typography>
 
          <FullStarIcon />
-      </RatingCont>
+      </Box>
 
-      <RatingChartBarContainer>
+      <Box className="rating-chart-bar-container">
          {RATINGS.map(({ label, progress }) => (
-            <RatingChartBar>
-               <RatingLabel>{label}</RatingLabel>
+            <Box className="rating-chart-bar">
+               <Typography className="rating-label">{label}</Typography>
 
-               <RatingProgressCont>
+               <Box className="rating-progress-cont">
                   <RatingProgress progress={progress} />
-               </RatingProgressCont>
+               </Box>
 
-               <RatingLabel>{progress}%</RatingLabel>
-            </RatingChartBar>
+               <Typography className="rating-label">{progress}%</Typography>
+            </Box>
          ))}
-      </RatingChartBarContainer>
+      </Box>
    </RatingChart>
 )
 
 export default Rating
 
-const RatingChart = styled(Box)(() => ({
+const RatingChart = styled(Box)(({ progress }) => ({
    border: '.0625rem solid #C4C4C4',
    borderRadius: '1rem',
    padding: '1.3125rem 2.5rem',
    width: 'fit-content',
-}))
 
-const RatingCont = styled(Box)(() => ({
-   margin: '0 0 .9375rem 0',
-   display: 'flex',
-   alignItems: 'center',
-   gap: '.625rem',
+   '& .rating-cont': {
+      margin: '0 0 .9375rem 0',
+      display: 'flex',
+      alignItems: 'center',
+      gap: '.625rem',
 
-   '& > p': {
-      fontSize: '1.5rem',
+      '& > p': {
+         fontSize: '1.5rem',
+      },
+
+      '& > svg': {
+         width: '1.9375rem',
+         height: '1.9375rem',
+      },
    },
-
-   '& > svg': {
-      width: '1.9375rem',
-      height: '1.9375rem',
+   '& .rating-chart-bar-container': {
+      color: '#363636',
    },
-}))
-
-const RatingChartBarContainer = styled(Box)(() => ({
-   color: '#363636',
-}))
-
-const RatingChartBar = styled(Box)(() => ({
-   display: 'flex',
-   alignItems: 'center',
-   justifyContent: 'space-between',
-   gap: '.875rem',
-}))
-
-const RatingLabel = styled(Typography)(() => ({
-   width: '.625rem',
-}))
-
-const RatingProgressCont = styled(Box)(() => ({
-   width: '20vw',
-   backgroundColor: '#d4cdd3',
-   overflow: 'hidden',
-   display: 'flex',
-   flexDirection: 'column',
-   height: '.1875rem',
+   '& .rating-chart-bar': {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: '.875rem',
+   },
+   '& .rating-label': {
+      width: '.625rem',
+   },
+   '& .rating-progress-cont': {
+      width: '20vw',
+      backgroundColor: '#d4cdd3',
+      overflow: 'hidden',
+      display: 'flex',
+      flexDirection: 'column',
+      height: '.1875rem',
+   },
+   '& .rating-progress': {
+      width: `${progress}%`,
+      display: 'flex ',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      transition: '0.4s ease-out',
+      height: '.625rem',
+      backgroundColor: '#4F7755',
+   },
 }))
 
 const RatingProgress = styled(Box)(({ progress }) => ({
