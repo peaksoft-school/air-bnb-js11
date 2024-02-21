7bae43c14a803c2e5f608a98c8ba687db1f67ccf
Jumanov Alisher <zumanovaliser814@gmail.com>
2024-02-21 14:27:11 +0600

    окшош импорторду чого жазабыз

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './store/store'
+import { injectStore } from './configs/axiosInstance'
 import Themes from './components/UI/Themes'
 import Notification from './components/UI/Notification'
 import App from './App'
-import { persistor, store } from './store/store'
-import { injectStore } from './configs/axiosInstance'
+import './index.css'
 
 injectStore(store)
 
