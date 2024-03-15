82d2f56030152588c4c1e4f7a942144dba8b9f8e
Aziat <abdimalikovaziat@gmail.com>
2024-03-15 11:56:37 +0600

    save

--- src/components/user/profile/MyAnnouncement.jsx
+++ src/components/user/profile/MyAnnouncement.jsx
@@ -1,119 +1,224 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {
    Box,
+   Checkbox,
+   Chip,
    FormControl,
-   FormGroup,
+   InputLabel,
+   ListItemText,
    MenuItem,
+   Rating,
    Select,
+   Stack,
    styled,
 } from '@mui/material'
 import { getAnnouncement } from '../../../store/slice/user/house/houseThunk'
 import LoadingSpinner from '../../UI/LoadingSpinner'
 import { UserNoDataImage } from '../../../assets/images'
 import Card from '../../UI/card/Card'
-import Checkbox from '../../UI/Checkbox'
 import RadioButton from '../../UI/RadioButton'
+
+const radioOptions = [
+   {
+      value: 'lowToHigh',
+      label: 'Low to high',
+   },
+   {
+      value: 'highToLow',
+      label: 'High to low',
+   },
+]
+
+const checkboxOptions = [
+   {
+      name: 'inWishList',
+      label: 'In wish list',
+   },
+   {
+      name: 'apartment',
+      label: 'Apartment',
+   },
+   {
+      name: 'house',
+      label: 'House',
+   },
+]
+
+const ratingOption = [
+   {
+      rating: 1,
+   },
+   {
+      rating: 2,
+   },
+   {
+      rating: 3,
+   },
+   {
+      rating: 4,
+   },
+   {
+      rating: 5,
+   },
+]
 
 const MyAnnouncement = () => {
    const dispatch = useDispatch()
    const { announcement, isLoading } = useSelector((state) => state.houses)
 
-   const [state, setState] = React.useState({
+   const [sortMenuOpen, setSortMenuOpen] = useState(false)
+   const [sortByRatingMenuOpen, setSortByRatingMenuOpen] = useState(false)
+   const [ratingState, setRatingState] = useState('')
+   const [sortState, setSortState] = useState([])
+   const [filterState, setFilterState] = useState({
       inWishList: false,
       apartment: false,
       house: false,
-      price: 'lowToHigh',
+      price: '',
    })
-   const [menuOpen, setMenuOpen] = React.useState(false)
 
-   const handleOpen = () => {
-      setMenuOpen(true)
-   }
+   const [filterChips, setFilterChips] = useState([])
 
-   const handleClose = () => {
-      setMenuOpen(false)
-   }
-
-   const handleChange = (event) => {
-      setState({
-         ...state,
-         [event.target.name]: event.target.checked,
-      })
-   }
-
-   const handlePriceChange = (event) => {
-      setState({
-         ...state,
-         price: event.target.value,
-      })
-   }
+   console.log(filterChips)
 
    useEffect(() => {
       dispatch(getAnnouncement())
-   }, [])
+   }, [dispatch])
+
+   const handleSortMenuToggle = () => {
+      setSortMenuOpen((prev) => !prev)
+   }
+
+   const handleSortByRatingMenuToggle = () => {
+      setSortByRatingMenuOpen((prev) => !prev)
+   }
+
+   const handleSortChange = (event) => {
+      const {
+         target: { value },
+      } = event
+      setSortState(typeof value === 'string' ? value.split(',') : value)
+   }
+
+   const handlePriceChange = (event) => {
+      setFilterState((prev) => ({
+         ...prev,
+         price: event.target.value,
+      }))
+   }
+
+   const handleRatingChange = (event) => {
+      setRatingState(event.target.value)
+   }
+
+   useEffect(() => {
+      const newChips = []
+
+      sortState.forEach((option) => {
+         newChips.push({ key: option, label: option })
+      })
+
+      if (filterState.price !== 'lowToHigh') {
+         const selectedSortOption = radioOptions.find(
+            (option) => option.value === filterState.price
+         )
+         newChips.push({ key: 'price', label: selectedSortOption?.label })
+      }
+
+      if (ratingState) {
+         newChips.push({ key: 'rating', label: `Rating: ${ratingState}` })
+      }
+
+      setFilterChips(newChips)
+   }, [filterState, ratingState, sortState])
+
+   console.log(sortState)
+
+   const handleDelete = (chipToDelete) => () => {
+      if (chipToDelete === 'rating') {
+         setRatingState('')
+      } else {
+         setFilterState((prev) => ({
+            ...prev,
+            [chipToDelete]: false,
+         }))
+      }
+   }
 
    if (isLoading) {
       return <LoadingSpinner />
    }
-
-   const radioOptions = [
-      {
-         value: 'lowToHigh',
-         label: 'Low to high',
-      },
-      {
-         value: 'highToLow',
-         label: 'High to low',
-      },
-   ]
-
-   const checkboxOptions = [
-      {
-         name: 'inWishList',
-         label: 'In wish list',
-      },
-      {
-         name: 'apartment',
-         label: 'Apartment',
-      },
-      {
-         name: 'house',
-         label: 'House',
-      },
-   ]
-
    return (
       <StyledAnnouncement>
-         <FormControl component="fieldset" className="form-control">
-            <Select open={menuOpen} onClose={handleClose} onOpen={handleOpen}>
-               <MenuItem value="">
-                  <em>All</em>
-               </MenuItem>
-
-               <FormGroup>
-                  {checkboxOptions.map((option) => (
-                     <Checkbox
-                        changeChecked={handleChange}
-                        checked={state[option.name]}
-                        label={option.label}
-                        name={option.name}
+         {announcement && !announcement.length > 0 ? (
+            <>
+               <FormControl className="form-control">
+                  <InputLabel id="sort">Sort</InputLabel>
+                  <Select
+                     multiple
+                     open={sortMenuOpen}
+                     onClose={handleSortMenuToggle}
+                     onOpen={handleSortMenuToggle}
+                     onChange={handleSortChange}
+                     value={sortState}
+                     labelId="sort"
+                     renderValue={(selected) => selected.join(', ')}
+                     label="sort"
+                  >
+                     <MenuItem value="All">All</MenuItem>
+                     {checkboxOptions.map((option) => (
+                        <MenuItem key={option.name} value={option.label}>
+                           <Checkbox
+                              color="warning"
+                              checked={sortState.indexOf(option.label) > -1}
+                           />
+                           <ListItemText primary={option.label} />
+                        </MenuItem>
+                     ))}
+                     <RadioButton
+                        options={radioOptions}
+                        value={filterState.price}
+                        onChange={handlePriceChange}
+                        name="price"
                      />
-                  ))}
-               </FormGroup>
+                  </Select>
+               </FormControl>
+               <FormControl className="form-control">
+                  <InputLabel id="sortByRatings">Sort by ratings</InputLabel>
 
-               <RadioButton
-                  options={radioOptions}
-                  value={state.price}
-                  onChange={handlePriceChange}
-                  name="price"
-               />
-            </Select>
-         </FormControl>
-         {announcement && announcement.length > 0 ? (
-            announcement.map((booking) => (
-               <Card key={booking.id} {...booking} />
-            ))
+                  <Select
+                     open={sortByRatingMenuOpen}
+                     onClose={handleSortByRatingMenuToggle}
+                     onOpen={handleSortByRatingMenuToggle}
+                     labelId="sortByRatings"
+                     label="Sort by ratings"
+                     value={ratingState}
+                     onChange={handleRatingChange}
+                  >
+                     <MenuItem value="">All</MenuItem>
+                     {ratingOption.map(({ rating }) => (
+                        <MenuItem key={rating} value={rating}>
+                           <Rating value={rating} readOnly />
+                        </MenuItem>
+                     ))}
+                  </Select>
+               </FormControl>
+               <Box>
+                  <Stack direction="row" spacing={1}>
+                     {filterChips.map((chip) => (
+                        <Chip
+                           key={chip.key}
+                           label={chip.label}
+                           onDelete={handleDelete(chip.key)}
+                        />
+                     ))}
+                  </Stack>
+               </Box>
+               {announcement.map((booking) => (
+                  <Card key={booking.id} {...booking} />
+               ))}
+            </>
          ) : (
             <img src={UserNoDataImage} alt="no house" />
          )}
@@ -124,11 +229,23 @@ const MyAnnouncement = () => {
 export default MyAnnouncement
 
 const StyledAnnouncement = styled(Box)(() => ({
-   display: 'flex',
    gap: '20px',
 
+   '& .MuiSelect-select': {
+      height: '20px',
+   },
+
    '& .form-control': {
-      width: '200px',
+      width: '270px',
+      margin: '0 10px 16px 0',
+
+      '& .Mui-focused': {
+         color: '#222',
+      },
+
+      '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+         border: '1px solid #484850',
+      },
    },
 
    '& > p': {
